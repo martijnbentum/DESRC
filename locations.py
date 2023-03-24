6cd587a6a2532f988e45e8f805aca67bd51d37df
martijnbentum <bentummartijn@gmail.com>
2023-03-24 15:27:03 +0100

    added function to make participant dict mapping pp_id to directory

--- locations.py
+++ locations.py
@@ -1,3 +1,6 @@
+import glob
+import os
+
 #change this to the directory of the corpus
 corpus_dir= '../../BAK/'
 
@@ -11,3 +14,14 @@ info_dir = other_files_dir + 'INFO/'
 log_dir = other_files_dir + 'log/'
 metadata_xml_dir = corpus_dir + 'XML_INFO/'
 
+fn = glob.glob(metadata_xml_dir + '*')
+participant_xml_dirs = [x for x in fn if os.path.isdir(x)]
+
+def participant_xml_dir_dict():
+    temp,o = {},{}
+    for d in participant_xml_dirs:
+        pp_id = int(d.split('PP')[-1])
+        temp[pp_id] = d + '/'
+    for k in sorted(temp.keys()):
+        o[k] = temp[k]
+    return o
