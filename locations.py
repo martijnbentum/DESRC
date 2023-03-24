398c76814dc9ff3abcfa0d6bd9e15fd4f5c08fc8
martijnbentum <bentummartijn@gmail.com>
2023-03-24 12:30:32 +0100

    module with directories of the DESRC corpus

---
+++ locations.py
@@ -0,0 +1,13 @@
+#change this to the directory of the corpus
+corpus_dir= '../../BAK/'
+
+#you do not need to change this
+eeg_dir = corpus_dir + 'EEG/'
+other_files_dir = corpus_dir + 'EEG_DATA_ifadv_cgn/'
+artefacts_dir = other_files_dir + 'ARTEFAC_ANNOTATIONS/'
+excluded_dir = other_files_dir + 'BADS_ANNOTATIONS/'
+ica_dir = other_files_dir + 'ICA_SOLUTIONS/'
+info_dir = other_files_dir + 'INFO/'
+log_dir = other_files_dir + 'log/'
+metadata_xml_dir = corpus_dir + 'XML_INFO/'
+
