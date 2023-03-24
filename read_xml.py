243d8c8d8481b74562fe8e80be3c29c44444a939
martijnbentum <bentummartijn@gmail.com>
2023-03-24 16:52:07 +0100

    changed layout of code; added function load field explanation for session block word etc.

--- read_xml.py
+++ read_xml.py
@@ -6,8 +6,8 @@ pp_dirs = locations.participant_xml_dir_dict()
 def load_all_participants(add_words = False):
     '''Load EEG experiment information for all participants.
     add_words       loads the word information of all words in the speech
-                    materials participants listened while their EEG was recorded
-                    (increases loading time)
+                    materials participants listened while their EEG 
+                    was recorded (increases loading time)
     '''
     participants = []
     for i in range(1,49):
@@ -36,6 +36,7 @@ def load_participant(pp_id = 1,add_words = False):
 
 def read_blocks(pp_id = 1, add_words = False,get_names = False):
     '''A block is a ~15 minute section of an EEG recording session
+    participant could take break after each block.
     '''
     blocks = []
     pp_dir = pp_dirs[pp_id]
@@ -50,8 +51,8 @@ def read_blocks(pp_id = 1, add_words = False,get_names = False):
     names = names.split(',')
     if get_names: return names
     integers = 'pp_id,block_number,st_sample,et_sample,duration_sample'
-    integers +=',sample_inacc,nallwords,ncontent_words,nartefacts,fid_st,fid_et'
-    integers += ',artefact_st,artefact_et,ica_remove_components'
+    integers +=',sample_inacc,nallwords,ncontent_words,nartefacts,fid_st'
+    integers += ',fid_et,artefact_st,artefact_et,ica_remove_components'
     integers = integers.split(',')
     floats = 'artefacts_duration,block_duration'.split(',')
     lists = 'fids,fid_st,fid_et,artefact_st,artefact_et'
@@ -80,7 +81,9 @@ def read_blocks(pp_id = 1, add_words = False,get_names = False):
 
 def read_participant(pp_id = 1):
     '''A participant participated in three EEG recorded session.
-    each session consisted of multiple blocks
+    each session consisted of multiple blocks.
+    Session were separated by at least one week.
+    Speech from one specific register (books, news, dialogues) was presented
     '''
     participant = dummy_object('participant')
     pp_dir = pp_dirs[pp_id]
@@ -89,7 +92,8 @@ def read_participant(pp_id = 1):
     names += ',names_sessions,nblocks_missing,names_block_missing'
     names += ',blocks_duration,artefacts_duration'
     names = names.split(',')
-    integers = 'pp_id,nallwords,ncontent_words,nartefacts,nblocks_missing'.split(',')
+    integers = 'pp_id,nallwords,ncontent_words,nartefacts,nblocks_missing'
+    integers = integers.split(',')
     floats = 'blocks_duration,artefacts_duration'.split(',')
     lists= 'dates_sessions,names_sessions'.split(',')
     xml = etree.fromstring(open(filename).read())
@@ -111,21 +115,22 @@ def read_sessions(pp_id = 1):
     sessions = []
     pp_dir = pp_dirs[pp_id]
     filename = pp_dir + 'sessions.xml'
-    names = 'pp_id,exp_type,name,experiment_name,session_number,n_eeg_recordings'
-    names += ',start_exp,end_exp,duration,nblocks,nallwords,ncontent_words'
+    names = 'pp_id,exp_type,name,experiment_name,session_number'
+    names += ',n_eeg_recordings,start_exp,end_exp,duration'
+    names += ',nblocks,nallwords,ncontent_words'
     names += ',nartifacts,fids,fids_missing,usability,answer_fn,log_fn'
-    names += ',vmrk_fn,vhdr_fn,eeg_fn,block_names,nblocks_missing,blocks_duration'
-    names += ',artefacts_duration'
+    names += ',vmrk_fn,vhdr_fn,eeg_fn,block_names,nblocks_missing'
+    names += ',blocks_duration,artefacts_duration'
     names = names.split(',')
     integers = 'pp_id,session_number,n_eeg_recordings,nblocks,nallwords'
     integers = ',ncontent_words,nartefacts,nblocks_missing'
     integers = integers.split(',')
     floats = 'blocks_duration,artefacts_duration'.split(',')
-    lists = 'fids_missing,usability,vmrk_fn,vhdr_fn,eeg_fn,block_names'.split(',')
+    lists = 'fids_missing,usability,vmrk_fn,vhdr_fn,eeg_fn,block_names'
+    lists = lists.split(',')
     nested_lists = ['fids']
     add_location = 'answer_fn,eeg_fn,log_fn,vhdr_fn,vmrk_fn'
     add_location = add_location.split(',')
-
     xml = etree.fromstring(open(filename).read())
     for s in xml.iter('session'):
         session = dummy_object('session')
@@ -135,7 +140,8 @@ def read_sessions(pp_id = 1):
             elif name in integers: o = int(text)
             elif name in floats: o = float(text)
             elif name in lists: o = text.split(',')
-            elif name in nested_lists: o = [fids.split('|') for fids in text.split(',')]
+            elif name in nested_lists: 
+                o = [fids.split('|') for fids in text.split(',')]
             else: o = text
             if name in add_location and o:
                 o = _add_corpus_location_to_filename(name)
@@ -153,7 +159,8 @@ def read_words(block_name = 'pp1_exp-o_bid-1'):
     Words are grouped within a block (~15 minutes of the EEG experiment)
     and blocks are grouped in a session (one ~90 minute recording session)
     word info can be found in this object
-    extra info can be found in the nested objects: phoneme_word, pos, ppl, stats 
+    extra info can be found in the nested objects: 
+    phoneme_word, pos, ppl, stats 
 
     Words can be read by providing a block name
     '''
@@ -163,10 +170,11 @@ def read_words(block_name = 'pp1_exp-o_bid-1'):
     word_dir = pp_dir  + 'WORDS/'
     filename = word_dir + block_name + '.xml'
     names = 'word_utf8_nocode,st_sample,et_sample,duration_sample,st,et,eol'
-    names += ',fid,sid,overlap,corpus,register,word,fid_st_sample,block_name'
-    names += ',word_index_in_block'
+    names += ',fid,sid,overlap,corpus,register,word,fid_st_sample'
+    names += ',block_name,word_index_in_block'
     names = names.split(',')
-    integers = 'st_sample,et_sample,fid_st_sample,word_index_in_block'.split(',')
+    integers = 'st_sample,et_sample,fid_st_sample,word_index_in_block'
+    integers = integers.split(',')
     floats = 'st,et'.split(',')
     booleans = ['overlap']
     xml = etree.fromstring(open(filename).read())
@@ -211,14 +219,16 @@ def _read_pos(xml_word):
     return pos
 
 def _read_stats(xml_word):
-    '''Information theoretic information. Language modelling information see ppl'''
+    '''Information theoretic information. 
+    Language modelling information see ppl'''
     stats= dummy_object('stats')
     s = xml_word.find('stats')
-    names = 'word_frequency,entropy,updated_entropy,cross_entropy,logprob,gate'
-    names += ',word_number,word_code,updated_logprob'
+    names = 'word_frequency,entropy,updated_entropy,cross_entropy'
+    names += ',logprob,gate,word_number,word_code,updated_logprob'
     names = names.split(',')
     integers = 'word_frequency,gate'.split(',')
-    floats = 'entropy,updated_entropy,cross_entropy,logprob,updated_logprob'.split(',') 
+    floats = 'entropy,updated_entropy,cross_entropy,logprob,updated_logprob'
+    floats = floats.split(',') 
     for name in names:
         text = s.find(name).text
         if text == 'NA' or text == '' or text == None: o = None
@@ -234,7 +244,8 @@ def _read_ppl(xml_word):
     p = xml_word.find('ppl')
     if p == None: return None
     names = 'ngram,oov,p,logprob,p_register,logprob_register,p_other1'
-    names += ',logprob_other1,p_other2,logprob_other2,word_id,word_index_sentence'
+    names += ',logprob_other1,p_other2,logprob_other2,word_id'
+    names += ',word_index_sentence'
     names = names.split(',')
     integers = 'ngram,word_index_sentence'.split(',')
     floats = 'p,logprob,p_register,logprob_register,p_other1,logprob_other1'
@@ -294,7 +305,9 @@ class dummy_object():
         self.name = name
 
     def __repr__(self):
-        m = self.object_type + ': ' + self.name
+        if '_help' in self.object_type: name = 'field explanations'
+        else: name = self.name
+        m = self.object_type + ': ' + name
         return m
 
     def __str__(self):
@@ -303,18 +316,41 @@ class dummy_object():
         x = len(max(list(d.keys()),key=len))
         m = bolunder +self.object_type+end + '\n' 
         for k in sorted(d.keys()):
-            if type(d[k]) == dummy_object: nested_objects.append(k)
+            if type(d[k]) == dummy_object and not k.startswith('_'): 
+                nested_objects.append(k)
             elif k == 'words' and type(d[k]) == list:
                 m += bold+ k.ljust(x + 3)+ end 
-                m += underline + 'word objects'+ end+ ': '+str(len(d[k])) +'\n'
+                m += underline + 'word objects'+ end
+                m += ': '+str(len(d[k])) +'\n'
             else:m += bold+ k.ljust(x + 3)+ end + str(d[k]) + '\n'
         if len(nested_objects) > 0:
-            m += '\n'+bold+'nested objects:'.ljust(x + 3)+end + ' '.join(nested_objects)
+            m += '\n'+bold+'nested objects:'.ljust(x + 3)+end 
+            m += ' '.join(nested_objects)
         return m
+
+    @property
+    def help(self):
+        '''show explanation for each field of an object.'''
+        if not hasattr(self,'_help'): 
+            self._help = make_help(self)
+        print(self._help)
+    
         
 
-def make_help(object_type,names):
-    xml = etree.fromstring(open('XML_INFO/info_explanation.xml').read())
+def make_help(obj):
+    '''create field explanation for an object (e.g. block)'''
+    object_type= obj.object_type
+    f = locations.metadata_xml_dir + 'info_explanation.xml'
+    helper = dummy_object(object_type+ '_help')
+    xml_help = etree.fromstring(open(f).read())
+    tags = [x.tag for x in xml_help.getchildren()]
+    if not object_type in tags: return 'no help available'
+    p = xml_help.find(object_type)
+    if p == None: return xml_help
+    for el in p.getchildren():
+        text = el.text.replace('\t',' ').replace('\n','')
+        setattr(helper,el.tag,text)
+    return helper 
     
 
 bold = '\033[1m'
@@ -326,11 +362,13 @@ end = '\033[0m'
         
 
 def _add_corpus_dir(f):
+    '''add corpus base dir to path if not present.'''
     if not f.startswith(locations.corpus_dir):
         f = locations.corpus_dir + f
     return f
 
 def _add_corpus_location_to_filename(f):
+    '''add corpus base dir to path(s) if not present.'''
     if type(f) == list:
         return [_add_corpus_dir(x) for x in f]
     return _add_corpus_dir(f)
